da1669c670d2f83abc9a3069715e4742c3ed0472
Juan Pablo <jpmansilla@live.com.ar>
2022-02-25 00:27:45 -0300

    BackendLaravel

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\ValidationException;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::post('login', function(Request $request){
+
+    $credentials = $request->validate([
+        'email' => ['required', 'email'],
+        'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+        $request->session()->regenerate();
+        return response()->json(Auth::user());
+    }
+
+    throw ValidationException::withMessages([
+        'message' => 'The email or password is wrong',
+    ]);
+} );
